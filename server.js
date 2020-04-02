e1bab428502554856c3cf9b0ff7a9b87808f8a42
Glitch (rimantas-file-metadata) <none>
2020-04-02 13:06:34 +0000

    🙈🗼 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+const multer = require("multer");
+// var upload = multer();
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.post("/api/fileanalyse", multer().single('upfile'), (req, res) => {
+  const {originalname, mimetype, size} = req.file;
+  res.json({"name":originalname,"type":mimetype,"size":size})
+})
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
